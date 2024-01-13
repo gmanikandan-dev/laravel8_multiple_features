cef29745d61fc0457678a5a0395d062e5af6f75a
gmanikandan-dev <manikandan.g@targetbay.com>
2024-01-13 17:24:17 +0530

    spatie Hastraits issue fixed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,15 @@ namespace App\Models;
   
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 
   
 class Post extends Model implements HasMedia
 {
- use HasMediaTrait;
+ use HasMediaTrait,HasFactory;
   
     protected $fillable = [
         'title',
